88c9024d767cd5051932413bcb6e54b81b579268
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2025-10-13 00:28:33 +0330

    winning method modified

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,5 +1,5 @@
 def winner_move(column_count: int, row_count: int, player_move: int, board: list):
-    for c in range(column_count - 2):
+    for c in range(column_count - 3):  
         for r in range(row_count):
             if (
                 board[c][r] == player_move
@@ -9,7 +9,7 @@ def winner_move(column_count: int, row_count: int, player_move: int, board: list
             ):
                 return True
 
-    # Check vertical locations for win
+   
     for c in range(column_count):
         for r in range(row_count - 3):
             if (
@@ -20,26 +20,27 @@ def winner_move(column_count: int, row_count: int, player_move: int, board: list
             ):
                 return True
 
-    # Check positively sloped diaganols
+ 
     for c in range(column_count - 3):
         for r in range(row_count - 3):
             if (
-                board[r][c] == player_move
-                and board[r + 1][c + 1] == player_move
-                and board[r + 2][c + 2] == player_move
-                and board[r + 3][c + 3] == player_move
+                board[c][r] == player_move         
+                and board[c+1][r+1] == player_move 
+                and board[c+2][r+2] == player_move 
+                and board[c+3][r+3] == player_move
             ):
                 return True
 
-    # Check negatively sloped diaganols
+
     for c in range(column_count - 3):
         for r in range(3, row_count):
             if (
-                board[r][c] == player_move
-                and board[r - 1][c + 1] == player_move
-                and board[r - 2][c + 2] == player_move
-                and board[r - 3][c + 3] == player_move
+                board[c][r] == player_move         
+                and board[c+1][r-1] == player_move 
+                and board[c+2][r-2] == player_move
+                and board[c+3][r-3] == player_move 
             ):
                 return True
 
     return False
+
