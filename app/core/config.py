ce7486b5e2d9e7cf892e962950eb6acdbcffb303
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2025-10-17 13:20:21 +0330

    the api_v1_str modified

--- app/core/config.py
+++ app/core/config.py
@@ -54,7 +54,7 @@ class Settings(BaseSettings):
 
     BACKEND_CORS_ORIGINS: List[str] = []
 
-    @field_validator("BACKEND_C ORS_ORIGINS", mode="before")
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
     @classmethod
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
