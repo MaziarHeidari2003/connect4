2f4dcde39d9b0ff0086f5b74fdff10ba94a96b3d
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2025-10-17 13:27:55 +0330

    the api_v1_str modified

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.utils.time_checker_job import scheduler_app
 
 app = FastAPI(title=settings.PROJECT_NAME)
 if settings.DEBUG:
-    app.openapi_url = settings.API_V1_STR
+    app.openapi_url = f"/connect4{settings.API_V1_STR}"
     app.setup()
 if settings.SUB_PATH:
     app.mount(f"{settings.SUB_PATH}", app)
